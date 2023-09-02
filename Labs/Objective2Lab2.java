afbbc62fc5bae711cf1c640fa756b11a52ca23b4
Parrisu <parris.a.jones@gmail.com>
2023-09-02 10:58:29 -0700

    complete Objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+		String name = "Parris Jones";
+		int age = 26;
+		String state = "California";
+
+		System.out.println("Name: "+name+" Age: "+age+" State: "+state);
+
+
+	}
+}
