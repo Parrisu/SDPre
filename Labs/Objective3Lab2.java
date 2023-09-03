2b10580c5ddff646b538f468916afe3346d53833
Parrisu <parris.a.jones@gmail.com>
2023-09-02 17:23:37 -0700

    complete Objective3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,17 @@
+public class Objective3Lab2 {
+	public static void main(String[] args) {
+		double side1, side2;
+
+		side1 = 10;
+		side2 = 8;
+		
+		double num1 = side1 * side1;
+		double num2 = side2 * side2;
+
+		double num3 = num1 + num2;
+
+		double result = Math.sqrt(num3);
+
+		System.out.println(result);	
+	}
+}
