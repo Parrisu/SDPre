b49be43e088ec0c9e5f232e3e8a219bb049ac984
Parrisu <parris.a.jones@gmail.com>
2023-09-04 09:09:26 -0700

    complete Objective7Lab1

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,12 @@
+public class Objective7Lab1 {
+	public static void main(String[] args) {
+		int counter = 10;
+		System.out.println(counter);
+
+
+		while(counter > 1) {
+			counter = counter - 1;
+			System.out.println(counter);
+		}
+	}
+}
