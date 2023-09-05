258fe6919cf8ba82ba2c1decd841a1c40f1ffc5e
Parrisu <parris.a.jones@gmail.com>
2023-09-05 12:16:50 -0700

    complete Objective9Lab1

---
+++ Labs/Objective9Lab1.java
@@ -0,0 +1,10 @@
+public class Objective9Lab1 {
+	public static void main(String[] args) {
+		printName();
+		// solution
+
+	}
+	public static void printName() {
+		System.out.println("Hello Parris");
+	}
+}
